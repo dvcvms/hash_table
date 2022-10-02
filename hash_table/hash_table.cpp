b7958ab8804252278f4b1785c5d3b8b971dfaad3
Max <tetraedrtrapecia@gmail.com>
2022-10-02 06:17:50 +0300

    -add auto increase size -add check size control

--- hash_table/hash_table.cpp
+++ hash_table/hash_table.cpp
@@ -9,6 +9,7 @@ class HashTable {
 private:
 
 	int capacity;
+	int fullness;
 	int** table;
 
 	int const_c, const_d;
@@ -19,9 +20,16 @@ public:
 	HashTable(int capacity);
 	int hashFunction(int key);
 	void insertItem(int key, int data);
+	void checkSize();
 
 	int getPrime(int value);
 	bool checkPrime(int value);
+	void set_constant_values();
+
+	void displayHash();
+
+private:
+	void setCapacity();
 
 };
 
@@ -33,6 +41,8 @@ HashTable::HashTable()
 	for (int i = 0; i < this->capacity; i++) {
 		table[i] = nullptr;
 	}
+
+	set_constant_values();
 }
 
 HashTable::HashTable(int capacity)
@@ -44,6 +54,8 @@ HashTable::HashTable(int capacity)
 	for (int i = 0; i < this->capacity; i++) {
 		table[i] = nullptr;
 	}
+
+	set_constant_values();
 }
 
 int HashTable::hashFunction(int key)
@@ -63,8 +75,10 @@ int HashTable::hashFunction(int key)
 
 void HashTable::insertItem(int key, int data)
 {
+	this->fullness++;
+	checkSize();
 	int index = hashFunction(key);
-	table[index][0] = data;
+	table[index] = new int(data);
 }
 
 int HashTable::getPrime(int value)
@@ -75,8 +89,11 @@ int HashTable::getPrime(int value)
 	if (value % 2 == 0)
 		value++;
 
-	while (!checkPrime(value))
+	do 
+	{
 		value += 2;
+	}
+	while (!checkPrime(value));
 
 	return value;
 }
@@ -93,11 +110,59 @@ bool HashTable::checkPrime(int value)
 	return true;
 }
 
-int main() {
+void HashTable::set_constant_values()
+{
+	this->const_c = rand() % this->capacity;
+	this->const_d = rand() % this->capacity;
+}
+
+void HashTable::checkSize()
+{
+	double fraction = (this->fullness / this->capacity) * 100;
+	
+	if (fraction > 75)
+		setCapacity();
+
+}
+
+void HashTable::setCapacity()
+{
+	int new_capacity = getPrime(this->capacity);
+	this->capacity = new_capacity;
+	set_constant_values();
+}
+
+void HashTable::displayHash()
+{
+	for (int i = 0; i < this->capacity; i++) 
+	{
+		cout << "table[" << i << "]";
+		cout << "-->" << this->table[i][0];
+		cout << endl;
+	}
+}
+
+int main() 
+{
+
+	int keys[] = { 1234567, 2345678, 3456789, 4567890, 5678901, 6789012, 7890123 };
+	int values[] = { 1, 2, 3, 4, 5, 6, 7 };
+	int size = sizeof(values) / sizeof(values[0]);
 
 
+	HashTable ht = HashTable(size);
+
+	for (int i = 0; i < size; i++)
+		ht.insertItem(keys[i], values[i]);
+
+	ht.displayHash();
+
+	ht.insertItem(123, 444);
+	ht.insertItem(124, 555);
+	ht.insertItem(125, 666);
+
+	ht.displayHash();
 
 	return 0;
-
 }
 
